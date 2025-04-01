0fd26713381bf09d001c97833ffbe587cb2b610a
Kaue de Almeida <kaualmeidapena@gmail.com>
2025-04-01 09:50:02 -0300

    Adicionando operadores aritméticos e lógicos

--- src/js/script.js
+++ src/js/script.js
@@ -1,48 +1,77 @@
-// //Declaração e variáveis
-// var nome = "fiap";
-// console.log(nome);
+// // //Declaração e variáveis
+// // var nome = "fiap";
+// // console.log(nome);
 
-// let idade = 18;
-// console.log(idade)
+// // let idade = 18;
+// // console.log(idade)
 
-// const sobrenome = "Cidade"
-// console.log(sobrenome)
+// // const sobrenome = "Cidade"
+// // console.log(sobrenome)
 
-// let aula;
-// console.log(aula)
-// //Indefinido
-// let valor = null;
-// console.log(valor)
-// //Nulo
-// let exemplo1={};
+// // let aula;
+// // console.log(aula)
+// // //Indefinido
+// // let valor = null;
+// // console.log(valor)
+// // //Nulo
+// // let exemplo1={};
 
-// let exemplo2=[];
+// // let exemplo2=[];
 
-// //Tipos de variaveis
-// let exemplo3 = 10;
-// console.log(typeof exemplo3)
-// let exemplo4 = "Aula";
-// console.log(typeof exemplo4)
-// let exemplo5 = true;
-// console.log(typeof exemplo5)
-// let exemplo6 = ["Huguinho", "zezinho", "luizinho"]
-// console.log(typeof exemplo6) 
+// // //Tipos de variaveis
+// // let exemplo3 = 10;
+// // console.log(typeof exemplo3)
+// // let exemplo4 = "Aula";
+// // console.log(typeof exemplo4)
+// // let exemplo5 = true;
+// // console.log(typeof exemplo5)
+// // let exemplo6 = ["Huguinho", "zezinho", "luizinho"]
+// // console.log(typeof exemplo6) 
 
-// //Conversões
+// // //Conversões
 
-// //float => inteiro
+// // //float => inteiro
 
-let numFloat = 123.879;
-console.log(parseInt(numFloat))
+// let numFloat = 123.879;
+// console.log(parseInt(numFloat))
 
-//string para float
-let numString = "547.987";
-console.log(parseFloat(numString))
+// //string para float
+// let numString = "547.987";
+// console.log(parseFloat(numString))
 
-//float => String
-let numFloat1 = 5690.60;
-console.log(numFloat.toString());
+// //float => String
+// let numFloat1 = 5690.60;
+// console.log(numFloat.toString());
 
-let numInt = 100;
-console.log(numInt.toString())
+// let numInt = 100;
+// console.log(numInt.toString())
 
+// //Métodos - Parte 1
+
+// //Legth - Verifica o tamanho da string
+// let frase = "O mundo da tecnologia";
+// console.log(frase.length)
+
+// //indexOf - retorna um tercho de um texto
+// let texto = "Programação sustentável";
+// console.log(texto.indexOf("a"))
+
+// //slice - Retorna parte de um texto apontando o inicio e o final
+// let info = "Processamento de ponta";
+// console.log(info.slice(17,22))
+
+//Operadores aritméticos
+const num1 = 10;
+const num2 = 20;
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+
+//Operadores lógicos
+const num3 = 20;
+const num4 = 40;
+console.log(num3 <= num4)
+console.log(num3 > num4 && num4 < 50)
+console.log(num3 > num4 || num4 > num3)
+console.log(num3 == num4 || num4 >= num3)
