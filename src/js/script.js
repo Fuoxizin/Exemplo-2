994b36e5f919580d6970b441eb998ec99bb5fac6
Kaue de Almeida <kaualmeidapena@gmail.com>
2025-04-01 09:10:04 -0300

    Declaração e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+//Declaração e variáveis
+var nome = "fiap";
+console.log(nome);
+
+let idade = 18;
+console.log(idade)
+
+const sobrenome = "Cidade"
+console.log(sobrenome)
+
+let aula;
+console.log(aula)
+//Indefinido
+let valor = null;
+console.log(valor)
+//Nulo
+let exemplo1={};
+
+let exemplo=[];
