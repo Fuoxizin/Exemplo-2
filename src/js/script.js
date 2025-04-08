4a22627e4b7cec250875f668375fe9d62d81a83f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-08 09:15:25 -0300

    Adicionando estruturas condicionais

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,36 @@
-// // //Declaração e variáveis
-// // var nome = "fiap";
-// // console.log(nome);
+// //Declaração e variáveis
+// var nome = "fiap";
+// console.log(nome);
 
-// // let idade = 18;
-// // console.log(idade)
+// let idade = 18;
+// console.log(idade)
 
-// // const sobrenome = "Cidade"
-// // console.log(sobrenome)
+// const sobrenome = "Cidade"
+// console.log(sobrenome)
 
-// // let aula;
-// // console.log(aula)
-// // //Indefinido
-// // let valor = null;
-// // console.log(valor)
-// // //Nulo
-// // let exemplo1={};
+// let aula;
+// console.log(aula)
+// //Indefinido
+// let valor = null;
+// console.log(valor)
+// //Nulo
+// let exemplo1={};
 
-// // let exemplo2=[];
+// let exemplo2=[];
 
-// // //Tipos de variaveis
-// // let exemplo3 = 10;
-// // console.log(typeof exemplo3)
-// // let exemplo4 = "Aula";
-// // console.log(typeof exemplo4)
-// // let exemplo5 = true;
-// // console.log(typeof exemplo5)
-// // let exemplo6 = ["Huguinho", "zezinho", "luizinho"]
-// // console.log(typeof exemplo6) 
+// //Tipos de variaveis
+// let exemplo3 = 10;
+// console.log(typeof exemplo3)
+// let exemplo4 = "Aula";
+// console.log(typeof exemplo4)
+// let exemplo5 = true;
+// console.log(typeof exemplo5)
+// let exemplo6 = ["Huguinho", "zezinho", "luizinho"]
+// console.log(typeof exemplo6) 
 
-// // //Conversões
+// //Conversões
 
-// // //float => inteiro
+// //float => inteiro
 
 // let numFloat = 123.879;
 // console.log(parseInt(numFloat))
@@ -60,18 +60,63 @@
 // let info = "Processamento de ponta";
 // console.log(info.slice(17,22))
 
-//Operadores aritméticos
-const num1 = 10;
-const num2 = 20;
-console.log(num1 + num2)
-console.log(num1 - num2)
-console.log(num1 * num2)
-console.log(num1 / num2)
+// //Operadores aritméticos
+// const num1 = 10;
+// const num2 = 20;
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
 
-//Operadores lógicos
-const num3 = 20;
-const num4 = 40;
-console.log(num3 <= num4)
-console.log(num3 > num4 && num4 < 50)
-console.log(num3 > num4 || num4 > num3)
-console.log(num3 == num4 || num4 >= num3)
+// //Operadores lógicos
+// const num3 = "20";
+// const num4 = 40;
+// console.log(num3 <= num4)
+// console.log(num3 > num4 && num4 < 50)
+// console.log(num3 > num4 || num4 > num3)
+// console.log(num4 == num3 || num4 > num3)
+// console.log(num4 == num3 && num4 > num3)
+
+// // Operadores de comparação
+
+// const num5 = 25;
+// const num6 = 35;
+
+// console.log(num5 == num6); //Compara
+// console.log(num5 === num6); //Compara e verifica o tipo da vaiavel
+// console.log(num5 !== num6); //Diferente
+
+// //Estrutura condicional
+
+// //IF/else
+
+// let valor1 = 100;
+
+// if(valor1 == 100){
+//     console.log("Vedadeiro");
+// }
+// else{
+//     console.log("Falso");
+// }
+
+// //if/else - encadeado ou aninhado
+
+// let idade = 14;
+// let naoAutorizar = "Entrada não autorizada"
+
+// if(idade < 14){
+//     console.log(naoAutorizar)
+// }
+// else if(idade >= 14 && idade < 18){
+//     console.log("Entrada autorizada!")
+// }
+// else{
+//     console.log(naoAutorizar)
+// }
+
+//Condição ternaria
+
+let valor2 = 300;
+
+let resultado = valor2 == 200 ? "Certo": "Errado";
+console.log(resultado) 
